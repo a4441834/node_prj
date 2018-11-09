001bb54d2ac3bcbf58f0d115c1a7b22d58a202c9
subbu <subbu@localhost.localdomain>
2018-11-09 16:33:52 -0600

    Test

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+
+const { mongoose } = require('./data/db');
+var employeeController = require('./controllers/employeeController');
+
+var app = express();
+app.use(bodyparser.json());
+
+var port = 8080;
+app.listen(port, ()=> console.log('Server Started at port:' +port));
+
+app.use('/employees', employeeController);
