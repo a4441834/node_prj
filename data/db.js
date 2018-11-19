b1694739e1a3845e8604208382c4d605541860a6
a4441834 <sub_mcse@yahoo.com>
2018-11-19 13:44:29 -0600

    Update db.js

--- data/db.js
+++ data/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://mongodb-user:mongodb-password@172.17.0.7:27017/sampledb', { useNewUrlParser: true }, (err)=>{
+mongoose.connect('mongodb://mongo-user:mongo-password@172.17.0.7:27017/sampledb', { useNewUrlParser: true }, (err)=>{
     if(!err)
         console.log('MongoDB connection succeeded');
     else
