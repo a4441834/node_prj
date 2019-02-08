2c875445b0d2ecaa7c59efddf24de8a9b8e00f18
Subbu Govindh <sub_mcse@yahoo.com>
2019-02-08 11:11:32 -0600

    Update db.js

--- data/db.js
+++ data/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
- mongoose.connect('mongodb://mongouser:mongopassword@172.30.32.246:27017/sampledb', { useNewUrlParser: true }, (err)=>{
+ mongoose.connect('mongodb://mongouser:Password1!@172.30.71.60:27017/sampledb', { useNewUrlParser: true }, (err)=>{
 // mongoose.connect('mongodb://user5Ng:k7EpSCPB@172.17.0.12:27017/sampledb', (err)=>{
     if(!err)
         console.log('MongoDB connection succeeded');
